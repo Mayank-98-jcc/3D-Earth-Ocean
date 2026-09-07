240fb599d05468e3d65ffb7062f1dd50ddf15495
Mayank-98-jcc <mayankthapliyal500@gmail.com>
2026-09-07 20:47:36 +0530

    Add longitude and latitude

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import EarthGlobe from './EarthGlobe.jsx'
 import './style.css'
+import './coordinates.css'
 createRoot(document.getElementById('root')).render(<EarthGlobe />)
